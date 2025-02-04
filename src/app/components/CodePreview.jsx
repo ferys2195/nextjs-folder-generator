c3d0981bb9798c884767f24ef82b3fd63ce5859c
Fery on Kantor <ferys2195@gmail.com>
2025-02-04 12:05:53 +0700

    add react icons

--- src/app/components/CodePreview.jsx
+++ src/app/components/CodePreview.jsx
@@ -1,32 +1,35 @@
 import React from "react";
+import { FiCopy } from "react-icons/fi";
 
 function CodePreview() {
   return (
-    <div className="mockup-code">
-      <pre>
-        <code>@echo off set "base_directory=%cd%" mkdir</code>
-      </pre>
-      <pre>
-        <code>"%base_directory%\Folder1\Subfolder1.1" mkdir</code>
-      </pre>
-      <pre>
-        <code>"%base_directory%\Folder1\Subfolder1.2" mkdir</code>
-      </pre>
-      <pre>
-        <code>"%base_directory%\Folder2\Subfolder2.1" mkdir</code>
-      </pre>
-      <pre>
-        <code>"%base_directory%\Folder2\Subfolder2.2" mkdir</code>
-      </pre>
-      <pre>
-        <code>
-          "%base_directory%\Folder3" echo Folders and subfolders created
-        </code>
-      </pre>
-      <pre>
-        <code>successfully!</code>
-      </pre>
-    </div>
+    <>
+      <div class="relative max-w-2xl mx-auto mt-24">
+        <div class="bg-gray-900 text-white p-4 rounded-md">
+          <div class="flex justify-between items-center mb-2">
+            <span class="text-gray-400">Code:</span>
+            <button
+              class="code bg-gray-800 hover:bg-gray-700 text-gray-300 px-3 py-1 rounded-md"
+              data-clipboard-target="#code"
+            >
+              Copy
+            </button>
+          </div>
+          <div class="overflow-x-auto">
+            <pre id="code" class="text-gray-300">
+              <code>
+                &lt;div class="flex justify-center items-center bg-blue-500
+                text-white p-4"&gt; &lt;h1 class="text-2xl"&gt;Hello, Tailwind
+                CSS!&lt;/h1&gt; &lt;/div&gt;
+              </code>
+            </pre>
+          </div>
+        </div>
+      </div>
+    </>
+
+    //<script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.8/clipboard.min.js"></script>
+    //<script>new ClipboardJS('.code');</script>
   );
 }
 
